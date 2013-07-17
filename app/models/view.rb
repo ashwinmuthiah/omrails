03fc2eae46600bf01d445f6937febce5b9e190af
Ashwin Muthiah <muthiah.ashwin2015@gmail.com>
2013-07-17 12:32:13 -0400

    Add User Model with Devise

--- app/models/view.rb
+++ app/models/view.rb
@@ -1,10 +1,8 @@
-class User < ActiveRecord::Base
+class View < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  # Below was commented out. Uncommented and added :password, 
-  #:password_confirmation, :email to avoid MassAssignment Security Error
-  attr_accessible :password, :password_confirmation, :email, :remember_me, :name
+  # attr_accessible :title, :body
 end
