608349c518b01df7bc3efd7db318fa5380a5b172
Ashwin Muthiah <muthiah.ashwin2015@gmail.com>
2013-07-18 17:17:06 -0400

    Add Pins Associations with Users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,8 @@ class Pin < ActiveRecord::Base
   #Validates a pin before it can be created. At least must be present, etc.
   #See more validations at rails validations on google
   validates :description, presence: true
+
+  #different relationships at rails associtaions
+  belongs_to :user
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ActiveRecord::Base
   # Below was commented out. Uncommented and added :password, 
   #:password_confirmation, :email to avoid MassAssignment Security Error
   attr_accessible :password, :password_confirmation, :email, :remember_me, :name
+
+  #different relationships at rails associtaions
+  has_many :pins, :dependent => :destroy
 end
